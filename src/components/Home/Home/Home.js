8c1984613d060527d8b25b02fd1c41c8cd15bbbd
MaheSajib <mahe.sajib@gmail.com>
2021-05-29 18:17:49 +0600

    updated git

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Header from '../Header/Header';
+import Video from '../Video/Video';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <Banner></Banner>
+            <Video></Video>
+            {/* Update */}
         </div>
     );
 };
