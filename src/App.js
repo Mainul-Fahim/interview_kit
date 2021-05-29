296887274c6f355dea46dcee371774dc9e044c65
Mainul-Fahim <36801303+Mainul-Fahim@users.noreply.github.com>
2021-05-29 18:01:35 +0600

    Interview Card section done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Home from './components/Home/Home/Home';
+import InterviewCard from './components/Home/InterviewCard/InterviewCard';
 
 function App() {
   return (
     <div className="App">
-      
+        <Home></Home>
+        <InterviewCard></InterviewCard>
     </div>
   );
 }
